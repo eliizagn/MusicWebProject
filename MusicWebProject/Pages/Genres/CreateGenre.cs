890d4e931920be823a310bab5541e4ce53c6cdc5
Элиза Норайровна Гаранян <nedlsg2001@gmail.com>
2023-11-03 23:20:16 +0300

    add include objects

---
+++ MusicWebProject/Pages/Genres/CreateGenre.cs
@@ -0,0 +1,31 @@
+﻿//создание певцов
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MusicWebProject.Data;
+using MusicWebProject.Data.Models;
+
+namespace MusicWebProject.Pages.Genres
+{
+    public class CreateGenre : PageModel
+    {
+
+        private MusicDbContext _musicDbContext;
+
+        public CreateGenre(MusicDbContext musicDbContext)
+        {
+            _musicDbContext = musicDbContext;
+        }
+        [BindProperty]
+        public Genre Genre {  get; set; }
+        // атрибут - настройки для сущности [BindProperty] 
+        // типы http запросов (post get put delete patch)
+        public IActionResult OnPost() 
+        {
+            _musicDbContext.Add(Genre);
+            _musicDbContext.SaveChanges();
+            return RedirectToPage("/Genres/Index");
+        }
+    }
+}
+
+
--- MusicWebProject/Pages/Genres/Index.cshtml.cs
+++ MusicWebProject/Pages/Genres/Index.cshtml.cs
@@ -1,41 +1,41 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.EntityFrameworkCore;
 using MusicWebProject.Data;
 using MusicWebProject.Data.Models;
 
-namespace MusicWebProject.Pages.Songs
+//стартовая страница для Singers
+
+namespace MusicWebProject.Pages.Genres
 {
     public class IndexModel : PageModel
     {
         [BindProperty(SupportsGet = true)]
         public string? SearchingString { get; set; }
+
         private MusicDbContext _musicDbContext;
 
+        //создать конструктор - передать входные параметры - присвоить значение поле на вход
+        // конструктор всегда имеет название класса
         public IndexModel(MusicDbContext musicDbContext)
         {
             _musicDbContext = musicDbContext;
         }
 
-        public List<Song> Songs { get; set; }
+        public List<Genre> Genres { get ; set; }
+
+        //метод 
         public void OnGet()
 
         {
             if (SearchingString != null)
             {
                 //Язык запросов LINQ - позволяет работать с коллекциями (таблица), Contains - метод, который позволяет сопоставить строчку ввода со строкой в таблице, х - это объект класса Singer??????
-                Songs = _musicDbContext.Songs.Where(x => x.Name.Contains(SearchingString))
-                    .Include(x=> x.Album)
-                    .Include(x => x.Singer)
-                    .ToList();
+                Genres = _musicDbContext.Genres.Where(x => x.Name.Contains(SearchingString)).ToList();
             }
-            else
-            {
-                Songs = _musicDbContext.Songs
-                    .Include(x => x.Album)
-                    .Include(x => x.Singer)
-                    .ToList();
+            else {
+                Genres = _musicDbContext.Genres.ToList();
             }
         }
     }
 }
+
